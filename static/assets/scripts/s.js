5a7ad8f0bdee04dc0c1224d89d05d2d8e333799c
11bunno11 <112693429+11bunno11@users.noreply.github.com>
2024-06-09 18:28:43 +0900

    Update s.js

--- static/assets/scripts/s.js
+++ static/assets/scripts/s.js
@@ -364,7 +364,7 @@ function AB() {
   if (!inFrame && !navigator.userAgent.includes("Firefox")) {
     const popup = open("about:blank", "_blank")
     if (!popup || popup.closed) {
-      alert("Please allow popups and redirects.")
+    
     } else {
       const doc = popup.document
       const iframe = doc.createElement("iframe")
